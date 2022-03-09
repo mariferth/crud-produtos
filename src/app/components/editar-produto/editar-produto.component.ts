6fdf951b1b7bc29bedb7c6b9bcae048911c44d88
mariferth <mari_jawwad@hotmail.com>
2022-03-08 23:31:42 -0300

    Update CRUD using services and validators P2

--- src/app/components/editar-produto/editar-produto.component.ts
+++ src/app/components/editar-produto/editar-produto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from 'src/app/models/produto';
 import { ProdutosService } from 'src/app/services/produtos.service';
@@ -9,13 +10,20 @@ import { ProdutosService } from 'src/app/services/produtos.service';
   styleUrls: ['./editar-produto.component.scss']
 })
 export class EditarProdutoComponent implements OnInit {
-  public nome : string | undefined;
-  public preco : number | undefined;
+  //public nome : string | undefined;
+  //public preco : number | undefined;
+  public formEditar : FormGroup;
   private indice : number = -1;
 
   constructor(private _router : Router, 
     private _actRoute : ActivatedRoute,
-    private _produtoService : ProdutosService) { }
+    private _produtoService : ProdutosService,
+    private _formBuilder : FormBuilder) {
+      this.formEditar = this._formBuilder.group({
+        nome : ["", [Validators.required, Validators.minLength(5)]],
+        preco : ["", [Validators.required]]
+      });
+     }
 
   ngOnInit(): void {
     this._actRoute.params.subscribe((parametros) => {
@@ -23,23 +31,41 @@ export class EditarProdutoComponent implements OnInit {
       if (parametros["indice"]) {
         this.indice = parametros["indice"];
         let produto = this._produtoService.getProduto(this.indice);
-        this.nome = produto.getNome();
-        this.preco = produto.getPreco();
+        //this.nome = produto.getNome();
+        //this.preco = produto.getPreco();
+        this.formEditar = this._formBuilder.group({
+          nome : [produto.getNome(), [Validators.required, Validators.minLength(5)]],
+          preco : [produto.getPreco(), [Validators.required]]
+        });
       }
     });
   }
 
+  private validarFormulario() {
+    for (let campos in this.formEditar.controls) {
+      this.formEditar.controls[campos].markAllAsTouched();
+    }
+  }
+
+  public submitForm (){
+    this.validarFormulario();
+    if (!this.formEditar.valid) {
+      return;
+    }
+    this.salvar();
+  }
+
   public salvar() {
     //console.log(this.nome + " " + this.preco);
-    if(!this.nome) {
+    /*if(!this.nome) {
       alert("Nome é obrigatório!");
       return;
     }
     if(!this.preco) {
       alert("Preço é obrigatório!");
       return;
-    }
-    let produto = new Produto(this.nome, this.preco);
+    }*/
+    let produto = new Produto(this.formEditar.controls["nome"].value, this.formEditar.controls["preco"].value);
     if(this._produtoService.editarProduto(this.indice, produto)){
       alert("Produto editado com sucesso!");
       this._router.navigate(["/listaDeProdutos"]);
